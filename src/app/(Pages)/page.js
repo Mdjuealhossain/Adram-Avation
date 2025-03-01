997a5359f6893dc462565ec1f4d59e9bc89dd18d
Md Jueal Hossain <juealdev@gmail.com>
2025-03-01 19:39:29 +0600

    first commit

---
+++ src/app/(Pages)/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className=" bg-black text-white p-12 text-H1 font-bold">
+            Next js, React and Tailwind config app
+        </div>
+    );
+};
+
+export default Home;
